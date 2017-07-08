dbd1ade771ea62ebf8a703db9096c296993101f1
Joshua Mabina <joshua@irabu.co.tz>
2017-07-08 18:43:10 +0300

    Create and center the make payment button

--- app/src/main/java/com/ipfsoftwares/mangi360/customer/MainActivity.java
+++ app/src/main/java/com/ipfsoftwares/mangi360/customer/MainActivity.java
@@ -7,46 +7,22 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.Editable;
-import android.text.InputFilter;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
-import com.bumptech.glide.Glide;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.appinvite.AppInviteInvitation;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.crash.FirebaseCrash;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity
         implements GoogleApiClient.OnConnectionFailedListener {
-
-    public static class MessageViewHolder extends RecyclerView.ViewHolder {
-        TextView messageTextView;
-        ImageView messageImageView;
-        TextView messengerTextView;
-        CircleImageView messengerImageView;
-
-        public MessageViewHolder(View v) {
-            super(v);
-            messageTextView = (TextView) itemView.findViewById(R.id.messageTextView);
-            messageImageView = (ImageView) itemView.findViewById(R.id.messageImageView);
-            messengerTextView = (TextView) itemView.findViewById(R.id.messengerTextView);
-            messengerImageView = (CircleImageView) itemView.findViewById(R.id.messengerImageView);
-        }
-    }
 
     private static final String TAG = "MainActivity";
     public static final String MESSAGES_CHILD = "messages";
@@ -62,18 +38,13 @@ public class MainActivity extends AppCompatActivity
     private GoogleApiClient mGoogleApiClient;
     private static final String MESSAGE_URL = "http://friendlychat.firebase.google.com/message/";
 
-    private Button mSendButton;
-    private RecyclerView mMessageRecyclerView;
+    private Button mMakePaymentButton;
     private LinearLayoutManager mLinearLayoutManager;
-    private ProgressBar mProgressBar;
-    private EditText mMessageEditText;
-    private ImageView mAddMessageImageView;
 
     // Firebase instance variables
 	private FirebaseAuth mFirebaseAuth;
 	private FirebaseUser mFirebaseUser;
 	private DatabaseReference mFirebaseReference;
-	private FirebaseRecyclerAdapter<ProductDAO, MessageViewHolder> mFirebaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,102 +74,11 @@ public class MainActivity extends AppCompatActivity
                 .addApi(Auth.GOOGLE_SIGN_IN_API)
                 .build();
 
-        // Initialize ProgressBar and RecyclerView.
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-        mMessageRecyclerView = (RecyclerView) findViewById(R.id.messageRecyclerView);
-        mLinearLayoutManager = new LinearLayoutManager(this);
-        mLinearLayoutManager.setStackFromEnd(true);
-
-        // Initialize firebase database instance variables
-        // and add all existing messages.
-        mFirebaseReference = FirebaseDatabase.getInstance().getReference();
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<ProductDAO, MessageViewHolder>(
-        		ProductDAO.class,
-        		R.layout.item_message,
-        		MessageViewHolder.class,
-        		mFirebaseReference.child(MESSAGES_CHILD)) {
-
-			@Override
-			public void populateViewHolder(MessageViewHolder viewHolder, ProductDAO productDAO, int position) {
-				mProgressBar.setVisibility(ProgressBar.INVISIBLE);
-				viewHolder.messageImageView.setVisibility(ImageView.GONE);
-
-				if (productDAO.getText() != null) {
-					viewHolder.messageTextView.setText(productDAO.getText());
-					viewHolder.messageTextView.setVisibility(TextView.VISIBLE);
-				} else {
-					// TODO: Implement fetch and display image messages
-				}
-
-				viewHolder.messengerTextView.setText(productDAO.getName());
-				if (productDAO.getPhotoUrl() != null) {
-					Glide.with(MainActivity.this)
-						.load(productDAO.getPhotoUrl())
-						.into(viewHolder.messengerImageView);
-				}
-			}
-        };
-
-        mFirebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-        	@Override
-        	public void onItemRangeInserted(int positionStart, int itemCount) {
-        		super.onItemRangeInserted(positionStart, itemCount);
-
-				int friendlyMessageCount = mFirebaseAdapter.getItemCount();
-        		int lastVisiblePosition = mLinearLayoutManager.findLastCompletelyVisibleItemPosition();
-        		if (lastVisiblePosition == -1 ||
-        				(positionStart >= (friendlyMessageCount - 1) &&
-        				lastVisiblePosition == (positionStart - 1))) {
-					mMessageRecyclerView.scrollToPosition(positionStart);
-        		}
-        	}
-        });
-
-        mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
-        mMessageRecyclerView.setAdapter(mFirebaseAdapter);
-
-        mMessageEditText = (EditText) findViewById(R.id.messageEditText);
-        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(mSharedPreferences
-                .getInt(CodelabPreferences.FRIENDLY_MSG_LENGTH, DEFAULT_MSG_LENGTH_LIMIT))});
-        mMessageEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (charSequence.toString().trim().length() > 0) {
-                    mSendButton.setEnabled(true);
-                } else {
-                    mSendButton.setEnabled(false);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-            }
-        });
-
-        mSendButton = (Button) findViewById(R.id.sendButton);
-        mSendButton.setOnClickListener(new View.OnClickListener() {
+        mMakePaymentButton = (Button) findViewById(R.id.makePaymentButton);
+        mMakePaymentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            	ProductDAO friendlyMessage = new ProductDAO(
-            			mMessageEditText.getText().toString(),
-            			mUsername,
-            			mPhotoUrl,
-            			null /* no image */);
-
-            	mFirebaseReference.child(MESSAGES_CHILD).push().setValue(friendlyMessage);
-            	mMessageEditText.setText("");
-            }
-        });
-
-        mAddMessageImageView = (ImageView) findViewById(R.id.addMessageImageView);
-        mAddMessageImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Select image for image message on click.
+                // TODO: implement onClickListener
             }
         });
     }
@@ -266,10 +146,6 @@ public class MainActivity extends AppCompatActivity
     		String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
     		Log.d(TAG, "Invitations sent: " + ids.length);
     	}
-    }
-
-    private void causeCrash() {
-    	throw new NullPointerException("Whhhopssies! Something went wrong.");
     }
 
     private void sendInvitation() {
